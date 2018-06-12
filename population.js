d4969332d3fdf39274fe159d7606e86607323719
Arth <arth.dharaskar97@gmail.com>
2018-06-12 14:41:35 +0530

    Update population.js

--- population.js
+++ population.js
@@ -1,5 +1,3 @@
-var maxfitP
-
 class Population{
     constructor(size){
         this.maxfit = 0;
@@ -29,21 +27,21 @@ class Population{
         }
 
     norm(){
-        this.sum_exp=0;
+        // this.sum_exp=0;
         // for (var i = 0;i<this.size;i++) {
         //      this.sum_exp += Math.exp(-this.blobs_pop[i].fitness)
         // }
         for (var i = 0;i<this.size;i++) {
             this.blobs_pop[i].fitness  /= this.maxfit;
+            // this.blobs_pop[i].fitness = map(this.blobs_pop[i].fitness,0,1,0,10);
             // createP(this.blobs_pop[i].fitness);
-
         }
         // for (var i = 0;i<this.size;i++) {
         //      this.blobs_pop[i].fitness = Math.exp(-this.blobs_pop[i].fitness)/this.sum_exp;
         // }
     }
 
-    selection(){
+    selection(m_rate){
         var new_pop =[];
 
         for (var i=0;i<this.size;i++)
@@ -51,19 +49,18 @@ class Population{
             var parentA = random(this.blobs_pop);
             var parentB = random(this.blobs_pop);
             while(true){
+
                 if(random(1)<parentA.fitness && random(1)< parentB.fitness){
                     var child_genes = parentA.crossover(parentB);
                     var child = new Blob();
                     child.dna.genes = child_genes[0];
                     child.color = child_genes[1];
-                    child.mutate(0.02);
-                    // console.log("Added");
+                    child.mutate(m_rate);
                     new_pop.push(child);
                     break;
                 }else{
                   parentA = random(this.blobs_pop);
                   parentB = random(this.blobs_pop);
-                  // console.log("Occured");
                 }
             }
 
