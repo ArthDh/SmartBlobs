26f1a4ad7172c9819924f6d673c26eba8323003e
Arth <arth.dharaskar97@gmail.com>
2018-06-12 14:42:06 +0530

    Update dna.js

--- dna.js
+++ dna.js
@@ -1,9 +1,9 @@
 class DNA{
-    constructor(timestep){
+    constructor(timestep,magnitude){
         this.genes=[];
         for (var i = 0; i <timestep; i++) {
             this.genes[i] = p5.Vector.random2D();
-            this.genes[i].setMag(0.2);
+            this.genes[i].setMag(magnitude);
         }
     }
 }
